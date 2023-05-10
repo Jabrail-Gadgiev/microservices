4c0dd408dca1c0879039b748da2ecdf83068842b
Jabrail-Gadgiev <114617792+Jabrail-Gadgiev@users.noreply.github.com>
2023-05-10 17:23:53 +0400

    Delete organization-service directory

--- organization-service/src/main/java/net/java/organizationservice/repository/OrganizationRepository.java
+++
@@ -1,9 +0,0 @@
-package net.java.organizationservice.repository;
-
-import net.java.organizationservice.dto.OrganizationDto;
-import net.java.organizationservice.entity.Organization;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganizationRepository extends JpaRepository<Organization, Long> {
-    Organization findByOrganizationCode(String organizationCode);
-}
--- organization-service/src/main/java/net/java/organizationservice/service/OrganizationService.java
+++
@@ -1,9 +0,0 @@
-package net.java.organizationservice.service;
-
-import net.java.organizationservice.dto.OrganizationDto;
-import net.java.organizationservice.entity.Organization;
-
-public interface OrganizationService {
-    OrganizationDto saveOrganization(OrganizationDto organizationDto);
-    OrganizationDto getOrganizationByCode(String organizationCode);
-}
