f687cd2fd524f7fcb19a2ca374e79f0521b0cbdb
Jabrail-Gadgiev <114617792+Jabrail-Gadgiev@users.noreply.github.com>
2023-05-10 17:23:41 +0400

    Delete employee-service directory

--- employee-service/src/main/java/net/java/employeeservice/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package net.java.employeeservice.repository;
-
-import net.java.employeeservice.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
