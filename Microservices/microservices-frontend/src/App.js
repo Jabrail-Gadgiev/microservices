c554335d063905a2168da5cdea39f00252569b08
Jabrail-Gadgiev <114617792+Jabrail-Gadgiev@users.noreply.github.com>
2023-05-11 11:46:30 +0400

    Implemented User Interface
    Implemented basic front-end to display information from microservices through ReactJS

---
+++ Microservices/microservices-frontend/src/App.js
@@ -0,0 +1,12 @@
+import EmployeeComponent from './component/EmployeeComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <EmployeeComponent></EmployeeComponent>
+    </div>
+  );
+}
+
+export default App;
